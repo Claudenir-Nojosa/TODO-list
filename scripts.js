dfff72ae49ce588d7ea67ab1e30ccaa5c5fbd6bc
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-03-27 08:53:14 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("forms");
+let linhas = "";
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const atividade = document.getElementById("nomeAtividade");
+    let linha = "<ul>";
+    linha += `<li>${atividade.value}</li>`;
+    linha += "</ul>";
+    linhas += linha;
+
+    const corpoLista = document.querySelector("ul");
+    corpoLista.innerHTML = linhas;
+    resetForms();
+});
+
+$("ul").click(function(){
+    $(this).toggleClass("concluido");
+})
+
+function resetForms() {
+    document.getElementById("forms").reset();
+}
